77ba83413d1c8fa8f56e8be1b222017d9b2cb413
LucaCoduriV <43602144+LucaCoduriV@users.noreply.github.com>
2021-07-11 02:02:49 +0200

    Update connectToGapps.ts

--- src/utils/connectToGapps.ts
+++ src/utils/connectToGapps.ts
@@ -2,9 +2,7 @@ import { Page } from "puppeteer";
 const { PendingXHR } = require("pending-xhr-puppeteer");
 
 export default async (page: Page, username: string, password: string): Promise<boolean> => {
-    const drop_down_value: string = "https://aai-logon.hes-so.ch/idp/shibboleth";
-    const pendingXHR = new PendingXHR(page);
-    //Va a la page de connexion
+    // Va a la page de connexion
     await page.goto(process.env.GAPS_LOGIN_URL, { waitUntil: "networkidle0" });
 
     // On remplit username/password pour se connecter
@@ -12,7 +10,6 @@ export default async (page: Page, username: string, password: string): Promise<b
     await page.type("#ident", username);
     await page.type("#mdp", password);
     await page.click(".bouton");
-    console.log("COUCOU");
     await Promise.race([page.waitForSelector("#menu_root"), page.waitForSelector(".fauxLogin")]);
 
     let isConnected: boolean = (await page.$(".fauxLogin")) == null;
