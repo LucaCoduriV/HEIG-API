850ce5c12e0a150e5e74425551d44a6bc88b2207
LucaCoduriV <43602144+LucaCoduriV@users.noreply.github.com>
2021-07-13 20:57:06 +0200

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -1,6 +1,6 @@
 export const browserOptions = () => {
     const options: any = {
-        headless: `${process.env.NODE_ENV}` == "production",
+        headless: true, //`${process.env.NODE_ENV}` == "production",
         args: ["--no-sandbox", "--disable-setuid-sandbox"],
     };
     if (process.env.CHROME_PATH) options["executablePath"] = `${process.env.CHROME_PATH}`;
