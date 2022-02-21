401c442384ac928a807f53b97a85c4dc42aa1eb8
Luca <coduri.vitor@gmail.com>
2022-02-21 22:33:39 +0100

    Changement de semestre

--- src/controllers/getHoraires.ts
+++ src/controllers/getHoraires.ts
@@ -1,32 +1,32 @@
-import { Request, Response } from "express";
-import Gaps from "../utils/gaps";
+import { Request, Response } from 'express';
+import Gaps from '../utils/gaps';
 
 enum Trimestre {
-    ETE = 1,
-    UN,
-    DEUX,
+  ETE = 1,
+  UN,
+  DEUX,
 }
 
 export default async (req: Request, res: Response) => {
-    let annee: number = 2021;
-    let trimestre: Trimestre = 1;
-    let type: number = 2; // 2 pour horaire étudiant
-    const gapsId = req.body.gapsId;
+  let annee: number = 2021;
+  let trimestre: Trimestre = 3;
+  let type: number = 2; // 2 pour horaire étudiant
+  const gapsId = req.body.gapsId;
 
-    const username: string = req.body.username as string;
-    const password: string = req.body.password as string;
+  const username: string = req.body.username as string;
+  const password: string = req.body.password as string;
 
-    try {
-        const resultats = await Gaps.get_horaires(
-            username,
-            password,
-            annee,
-            trimestre,
-            gapsId,
-            type
-        );
-        return res.send(resultats);
-    } catch (e) {
-        return res.status(500).send(e.message);
-    }
+  try {
+    const resultats = await Gaps.get_horaires(
+      username,
+      password,
+      annee,
+      trimestre,
+      gapsId,
+      type,
+    );
+    return res.send(resultats);
+  } catch (e) {
+    return res.status(500).send(e.message);
+  }
 };
