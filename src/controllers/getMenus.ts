db5eaf20d372fe31a318866a5fcaa5afbf7b0e2f
Luca <coduri.vitor@gmail.com>
2022-03-30 22:04:13 +0200

    Added fallback for menus

--- src/controllers/getMenus.ts
+++ src/controllers/getMenus.ts
@@ -3,6 +3,7 @@ import { StatusCodes } from 'http-status-codes';
 import intranet from '../utils/intranet';
 import { parse } from 'node-html-parser';
 import xlsx from 'xlsx';
+import axios from 'axios';
 
 export default async (req: Request, res: Response) => {
   if (!process.env.HEIG_USERNAME || !process.env.HEIG_PASSWORD) {
@@ -10,6 +11,8 @@ export default async (req: Request, res: Response) => {
       .status(StatusCodes.UNAUTHORIZED)
       .send('Please enter your HEIG credentials in your .env file.');
   }
+
+  const days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday'];
 
   try {
     // Get Excel file link
@@ -55,7 +58,6 @@ export default async (req: Request, res: Response) => {
         };
       });
 
-    const days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday'];
     const menus: Menus = {};
     let index = 0;
 
@@ -69,6 +71,47 @@ export default async (req: Request, res: Response) => {
       index += 7;
     }
 
+    return res.status(StatusCodes.OK).send(menus);
+  } catch (e) {
+    console.log(e);
+  }
+
+  try {
+    let todayIndex = new Date().getDay() - 1;
+    let result = await axios.get('https://apix.blacktree.io/top-chef/today');
+    let menus: Menus = {
+      monday: {
+        tradition: ['Seul le menu du jour est disponible.'],
+        vegetarien: ['Seul le menu du jour est disponible.'],
+      },
+      tuesday: {
+        tradition: ['Seul le menu du jour est disponible.'],
+        vegetarien: ['Seul le menu du jour est disponible.'],
+      },
+      wednesday: {
+        tradition: ['Seul le menu du jour est disponible.'],
+        vegetarien: ['Seul le menu du jour est disponible.'],
+      },
+      thursday: {
+        tradition: ['Seul le menu du jour est disponible.'],
+        vegetarien: ['Seul le menu du jour est disponible.'],
+      },
+      friday: {
+        tradition: ['Seul le menu du jour est disponible.'],
+        vegetarien: ['Seul le menu du jour est disponible.'],
+      },
+    };
+
+    menus[days[todayIndex]] = {
+      tradition: [
+        ...result.data.menus[0].mainCourse,
+        result.data.menus[0].dessert,
+      ],
+      vegetarien: [
+        ...result.data.menus[1].mainCourse,
+        result.data.menus[1].dessert,
+      ],
+    };
     return res.status(StatusCodes.OK).send(menus);
   } catch (e) {
     console.log(e);
