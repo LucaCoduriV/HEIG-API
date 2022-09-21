96efb6c7bba70fb606a5898f86d4e4e0ab9bece7
Luca Coduri <coduri.vitor@gmail.com>
2022-09-21 18:40:23 +0200

    new message at startup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import dotenv from "dotenv";
-import express from "express";
-import router from "./routes";
-import morgan from "morgan";
-import cors from "cors";
-import { json } from "body-parser";
-import { RsaManager } from "./utils/rsaUtils";
+import dotenv from 'dotenv';
+import express from 'express';
+import router from './routes';
+import morgan from 'morgan';
+import cors from 'cors';
+import { json } from 'body-parser';
+import { RsaManager } from './utils/rsaUtils';
 
 dotenv.config();
 
@@ -15,9 +15,9 @@ RsaManager.getInstance();
 
 app.use(cors());
 app.use(json());
-app.use(morgan(NODE_ENV == "development" ? "dev" : "combined"));
+app.use(morgan(NODE_ENV == 'development' ? 'dev' : 'combined'));
 app.use(router);
 
 app.listen(PORT, () => {
-    console.log(`server started at http://localhost:${PORT}`);
+  console.log(`HEIG API started at http://localhost:${PORT}`);
 });
