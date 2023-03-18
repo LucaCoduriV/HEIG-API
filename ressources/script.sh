1fec590255e5756896286bc8d79a7b0d795266fb
Luca <43602144+LucaCoduriV@users.noreply.github.com>
2023-03-18 12:11:23 +0100

    Update script.sh

--- ressources/script.sh
+++ ressources/script.sh
@@ -2,7 +2,7 @@
 
 for i in {0..300}
 do
-    curl "https://username:password.ever@gaps.heig-vd.ch/consultation/horaires/?annee=2022&trimestre=1&type=4&id=$i&icalendarversion=2&individual=1" --output ./icals/$i.ical
+    curl "https://username:password@gaps.heig-vd.ch/consultation/horaires/?annee=2022&trimestre=1&type=4&id=$i&icalendarversion=2&individual=1" --output ./icals/$i.ical
 done
 
 
